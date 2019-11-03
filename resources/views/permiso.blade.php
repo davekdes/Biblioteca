fdbf6bfa9f531678dc6c327ea3009ec18b7f100c
Job Biblioteca <davekdes@gmail.com>
2019-11-02 23:19:44 -0600

    Trabajo con Rutas

---
+++ resources/views/permiso.blade.php
@@ -0,0 +1,8 @@
+{{-- Sistema de permisos del sistema {{$nombre. ' '}} {{$slug}} --}}
+
+{{-- <a href="{{url('admin/sistema/permiso')}}"> Ir a permisos </a> --}}
+
+{{-- <a href="{{route('permiso')}}"> Ir a permisos </a>
+ --}}
+
+ Hola {{$nombre}}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,26 @@
 |
 */
 
+use App\Http\Controllers\PermisoController;
+
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+/*
+Route::get('foo', function () {
+  return 'Hello World';
+});
+*/
+
+//Route::get('permiso', 'PermisoController@index');
+//Route::view('permiso', 'permiso');
+
+//Route::get('permiso/{nombre}/{slug?}', 'PermisoController@index');
+//Route::get('admin/sistema/permiso', 'PermisoController@index')->name('permiso');
+
+Route::get('permiso/{nombre}', function ($nombre) {
+    return $nombre;
+})->where('nombre', '[0-9]+')->name('permiso');
